750a80f60a3f7b7b662ef031a9db03c2466c43fd
Koen1237 <koen.vaningen@wur.nl>
2024-03-14 14:57:01 +0100

    Added copyright message to class_logger, removed testing code

--- fermo_core_extras/mibig_spectral_library/data_processing/class_logger.py
+++ fermo_core_extras/mibig_spectral_library/data_processing/class_logger.py
@@ -1,3 +1,26 @@
+"""Enables colored logging throughout the mibig_spectral_library pipeline
+
+Copyright (c) 2022 to present Koen van Ingen, Mitja M. Zdouc, PhD and individual
+ contributors.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 import logging
 from typing import Self
 import sys
@@ -53,12 +76,3 @@ class Logger(BaseModel):
         logger.addHandler(file_handler)
 
         return logger
-
-
-if __name__ == "__main__":
-    argdict = {"logging_level": "DEBUG", "output_folder": "s_output"}
-    testie = Logger(**argdict)
-    this_logger = testie.enable_logging()
-    this_logger.info("Test info")
-    this_logger.error("Test error")
-    this_logger.critical("Test critical")
